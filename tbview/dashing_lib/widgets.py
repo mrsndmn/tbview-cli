32c75a1c4d685194acac979d8306d056d02110ec
Dmitrii Tarasov <d.tarasov@airi.net>
2025-09-23 20:42:01 +0300

    fix widgets

--- tbview/dashing_lib/widgets.py
+++ tbview/dashing_lib/widgets.py
@@ -61,5 +61,21 @@ class SelectionTile(Text):
         self.text = '\n'.join(styled_options)
     
     def _display(self, tbox, parent):
-        self._apply_options_to_text(tbox)
-        super()._display(tbox, parent)
+        # Render options without wrapping to avoid breaking ANSI sequences
+        tbox = self._draw_borders_and_title(tbox)
+        t = tbox.t
+        dx = 0
+        for i, opt in enumerate(self._options):
+            if dx >= tbox.h:
+                break
+            visible_text = opt[:tbox.w]
+            styled = (t.on_white if i == self._current else t.white)(visible_text)
+            print(
+                t.move(tbox.x + dx, tbox.y)
+                + styled
+                + " " * (tbox.w - len(visible_text))
+            )
+            dx += 1
+        while dx < tbox.h:
+            print(t.move(tbox.x + dx, tbox.y) + " " * tbox.w)
+            dx += 1
